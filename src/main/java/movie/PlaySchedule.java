08430d516852a64672c0a5ca4d6e043cec033929
hoseong <hoseong691@gmail.com>
2025-09-18 18:49:06 +0900

    feat: 중복 좌석 예매 방지 기능 구현

--- src/main/java/movie/PlaySchedule.java
+++ src/main/java/movie/PlaySchedule.java
@@ -1,12 +1,16 @@
 package movie;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaySchedule {
 
     private final Movie movie;
     private final LocalDateTime startTime;
     private final int seatSize;
+
+    private final List<Seat> bookedSeats = new ArrayList<>();
 
     public PlaySchedule(Movie movie, LocalDateTime startTime, int seatSize) {
         this.movie = movie;
@@ -22,6 +26,16 @@ public class PlaySchedule {
     }
     public int getSeatSize() {
         return seatSize;
+    }
+
+    public void processReservation(Reservation reservation) {
+        for(Seat seat : reservation.getSeats()) {
+            if (this.bookedSeats.contains(seat)) {
+                throw new IllegalArgumentException("이미 예약된 좌석입니다.");
+            }
+        }
+
+        this.bookedSeats.addAll(reservation.getSeats());
     }
 
 }
